7727173331b0aa70d1d47c39bf88f07626e0a458
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2019-12-26 20:26:01 -0300

    added section projects

--- js/index.js
+++ js/index.js
@@ -17,3 +17,22 @@
     }, false);
   }
 })();
+
+
+$(document).ready(function () {
+  function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+  
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+  }
+  
+  if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+  }
+});
